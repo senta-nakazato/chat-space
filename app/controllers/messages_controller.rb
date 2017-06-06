70cd672197bccfb8d3fd6f695ec8035aa9bd87a8
仲里専太 <senta@sentaMacBook-Pro.local>
2017-06-06 16:57:02 +0900

    create function to send message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,12 +3,23 @@ class MessagesController < ApplicationController
 
   def index
     @groups = current_user.groups
+    @users = @group.users
+    @message = Message.new
     @messages = Message.where(group_id: @group.id)
+  end
+
+  def create
+    Message.create(message_params)
+    redirect_to :index
   end
 
   private
   def get_group
     @group = Group.find(params[:group_id])
+  end
+
+  def message_params
+    params.permit(:text).merge(group_id: params[:group_id], user_id: current_user.id)
   end
 end
 
