9cdcd7b5dc9f9f73050fdf91f4ce75f5e1f19036
仲里専太 <senta@sentaMacBook-Pro.local>
2017-06-11 16:56:02 +0900

    setup how to log_in on test_development

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,6 +3,8 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'devise'
+require File.expand_path("spec/controller_macros.rb")
 
 ActiveRecord::Migration.maintain_test_schema!
 
@@ -12,5 +14,7 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
+  config.include Devise::TestHelpers, type: :controller
+  config.include ControllerMacros, type: :controller
 end
 
