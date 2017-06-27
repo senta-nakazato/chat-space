4d31af6ab467a19b406698be840fc3717e4126f5
仲里専太 <senta@sentaMacBook-Pro.local>
2017-06-27 22:04:46 +0900

    create function to search user

---
+++ app/assets/javascripts/group.js
@@ -0,0 +1,63 @@
+$(document).on('turbolinks:load', function(){
+  function searchUserResult(user) {
+   var html = `<div class="chat-group-user clearfix">
+                <p class="chat-group-user__name">
+                  ${ user.name }
+                  <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id= ${ user.id } data-user-name= ${ user.name }>
+                    追加
+                  </div>
+                </p>
+              </div>`;
+   return html;
+  }
+
+  function addUserList(id, name){
+    var html = `<div class="chat-group-user clearfix" id="chat-group-user-${id}">
+               <input type="hidden" name="group[user_ids][]" value="${id}">
+                 <p class="chat-group-user__name">
+                   ${name}
+                   <div class="user-search-remove chat-group-user__btn chat-group-user__btn--remove" data-user-id=${id}>
+                     削除
+                   </div>
+                 </p>
+             </div>`;
+   return html;
+  }
+
+  $("#user-search-field").on("keyup", function() {
+    var input = $(this).val();
+
+    $.ajax({
+      type: 'GET',
+      url: '/users/search',
+      data: { keyword: input },
+      dataType: 'json'
+    })
+   .done(function(users) {
+     if (users.length !== 0) {
+      var html = "";
+      $.each(users, function(index, user){
+         html += searchUserResult(user);
+         $("#user-search-result").empty();
+       });
+       $("#user-search-result").html(html);
+     } else {
+       flash("名前がありません！");
+     }
+    })
+   });
+
+
+  $(document).on('click', '.user-search-add', function(){
+    var id = $(this).data('user-id');
+    var name = $(this).data('user-name');
+    $(this).parent().remove();
+    var html = addUserList(id, name);
+    $('#user-search-result').append(html);
+  });
+
+  $(document).on('click', '.user-search-remove', function(){
+    $(this).parent().remove();
+  });
+});
+
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,14 @@ class UsersController < ApplicationController
     end
   end
 
+  def search
+    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").where.not(id: current_user.id)
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
+
   private
   def user_params
     params.require(:user).permit(:name, :email)
